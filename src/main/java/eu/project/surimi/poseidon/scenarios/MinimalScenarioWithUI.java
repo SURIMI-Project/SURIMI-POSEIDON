d7dc2bafea09f6b0a9d7e0291b2c75da4388a25b
Nicolas Payette <nicolaspayette@gmail.com>
2025-06-25 14:06:05 +0100

    add minimal UI for minimal scenario
    https://github.com/Official-EwE/SURIMI-project/issues/98

---
+++ src/main/java/eu/project/surimi/poseidon/scenarios/MinimalScenarioWithUI.java
@@ -0,0 +1,61 @@
+/*
+ * POSEIDON: an agent-based model of fisheries
+ * Copyright (c) 2025, University of Oxford.
+ *
+ * University of Oxford means the Chancellor, Masters and Scholars of the
+ * University of Oxford, having an administrative office at Wellington
+ * Square, Oxford OX1 2JD, UK.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package eu.project.surimi.poseidon.scenarios;
+
+import uk.ac.ox.poseidon.core.Scenario;
+import uk.ac.ox.poseidon.gui.DisplayWrapper2D;
+import uk.ac.ox.poseidon.gui.ScenarioWithUI;
+import uk.ac.ox.poseidon.gui.portrayals.BathymetryFieldPortrayalFactory;
+
+import java.util.List;
+
+import static java.awt.Color.WHITE;
+
+public class MinimalScenarioWithUI extends ScenarioWithUI {
+    public MinimalScenarioWithUI(
+        final Scenario scenario
+    ) {
+        super(
+            scenario,
+            List.of(
+                new DisplayWrapper2D(
+                    "Catalan Mediterranean Sea",
+                    List.of(
+                        new BathymetryFieldPortrayalFactory(
+                            scenario.component("bathymetricGrid")
+                        )
+                    ),
+                    600,
+                    600,
+                    WHITE
+                )
+            )
+        );
+    }
+
+    public static void main(final String[] args) {
+        final MinimalScenarioWithUI minimalScenarioWithUI =
+            new MinimalScenarioWithUI(new MinimalScenario().get());
+        minimalScenarioWithUI.createController();
+    }
+}
