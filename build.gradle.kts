96b41aa96e042645cd77aa0e4135051b4a7e952c
Nicolas Payette <nicolaspayette@gmail.com>
2026-04-21 11:00:16 +0100

    update SURIMI protocol version

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation(libs.commons.io)
     implementation(libs.bundles.opentelemetry)
     compileOnly("${libs.spotbugs.annotations.get()}:${spotbugs.toolVersion.get()}")
-    implementation("build.buf.gen:surimi_surimi-protocol_grpc_java:1.80.0.1.20260319121605.32d88b4cd30c")
+    implementation("build.buf.gen:surimi_surimi-protocol_grpc_java:1.80.0.1.20260420084716.70c35458b1f4")
     testImplementation(libs.jqwik)
     testImplementation(libs.assertj)
     testImplementation(libs.mockito)
