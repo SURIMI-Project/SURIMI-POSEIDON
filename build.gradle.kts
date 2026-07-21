67cba36ed8d66d5a97f8ffa396f8879b9d5e7dce
Nicolas Payette <nicolaspayette@gmail.com>
2026-07-21 13:24:20 +0100

    Allow Unsafe memory access for protobuf use

--- build.gradle.kts
+++ build.gradle.kts
@@ -131,7 +131,7 @@ tasks.named<Test>("test") {
     jvmArgumentProviders.add(mockitoAgentArgs)
     jvmArgs(
         "--enable-native-access=ALL-UNNAMED",
-        "--sun-misc-unsafe-memory-access=deny",
+        "--sun-misc-unsafe-memory-access=allow",
         "-Dio.grpc.netty.shaded.io.netty.noUnsafe=true"
     )
 }
